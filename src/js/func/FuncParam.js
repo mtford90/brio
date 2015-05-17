659b4e837883abfea7b594ad83abb1f9299632d8
Michael Ford <mtford@gmail.com>
2015-05-17 13:22:35 +0100

    add logic to deal with optional params

--- src/js/func/FuncParam.js
+++ src/js/func/FuncParam.js
@@ -5,11 +5,38 @@ var FuncParam = React.createClass({
     var name = this.props.name,
       def = this.props.def;
 
+
+    var heading = name + ' ' + '<' + def.type + '>';
+    if (def.optional) {
+      heading = '[' + heading + ']';
+    }
     return (
-      <a href='#'>
-        {name}
-      </a>
+      <div>
+        <span>{def.optional ? '[' : ''}
+          <a ref="a" href='#' className="highlighted-tooltip">
+            {name}
+          </a>
+        {def.optional ? ']' : ''}</span>
+
+        <div ref="tooltipContents" style={{display: 'none'}}>
+          <h5>{heading}</h5>
+        </div>
+      </div>
+
     )
+  },
+  componentDidMount: function () {
+    var $a = $(this.refs['a'].getDOMNode());
+    var $content = $(this.refs['tooltipContents'].getDOMNode()).clone();
+    $content.css('display', 'block');
+
+    console.log('$content', $content);
+    $a.tooltipster({
+      theme: 'my-custom-theme',
+      position: 'bottom-right',
+      content: $content
+    });
+
   }
 });
 
