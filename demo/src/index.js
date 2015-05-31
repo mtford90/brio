336cdea5cfbfe07ead9f530a44b8d35f0a733a14
Michael Ford <mtford@gmail.com>
2015-05-31 11:28:49 +0100

    configure demo

---
+++ demo/src/index.js
@@ -0,0 +1,117 @@
+brio({
+  id: 'app',
+  docs: {
+    title: 'Siesta',
+    pages: {
+      Documentation: {
+        Collections: [
+          {
+            type: 'paragraph',
+            content: 'A collection organises a set of models. For example we could create a collection' +
+            ' for organising models that represent resources on Github.'
+          },
+          {
+            type: 'function',
+            name: 'app.collection',
+            parameters: {
+              name: {
+                type: 'String',
+                name: 'The name of your collection',
+                optional: false
+              }
+            },
+            examples: [
+              {
+                name: 'Creating a collection',
+                description: '',
+                code: function (done) {
+                  var app = siesta.app('My App'),
+                    Github = app.collection('Github');
+
+                  done();
+                }
+              }
+            ]
+          }
+        ],
+        Serialisation: [
+          {
+            type: 'paragraph',
+            content: 'Serialisation is the process of getting a model ' +
+            'instance ready for conversion into a data transfer format like JSON e.g. ' +
+            'eliminating circular references.'
+          },
+          {
+            type: 'function',
+            name: 'ModelInstance.serialise',
+            description: 'Use the serialise function to prepare your instances for use ' +
+            'in a data transfer format such as JSON.',
+            parameters: {
+              model: {
+                type: 'Model',
+                description: 'A siesta model',
+                optional: false
+              },
+              opts: {
+                type: 'Object',
+                description: 'Options',
+                optional: true,
+                keys: {
+                  fields: {
+                    type: 'Array<String>',
+                    description: 'which fields to serialise.'
+                  },
+                  nullAttributes: {
+                    type: 'boolean',
+                    description: 'should null attributes be serialised?'
+                  },
+                  nullRelationships: {
+                    type: 'boolean',
+                    description: 'should null relationships be serialised?'
+                  }
+                }
+              }
+            },
+            examples: [
+              {
+                name: 'Specifying fields',
+                description: 'In the below example we explictly specify which ' +
+                'fields can be serialised.',
+                code: function (done) {
+                  var app = siesta.app('My App'),
+                    MyCollection = app.collection('MyCollection');
+
+                  var MyModel = MyCollection.model({
+                    name: 'MyModel',
+                    attributes: ['field1', 'field2'],
+                    serialisableFields: ['field1']
+                  });
+
+                  MyModel.graph({
+                    field1: 1,
+                    field2: 2
+                  }).then(function (instance) {
+                    var s = siesta.serialiser(MyModel);
+                    var serialised = s.data(instance),
+                      json = JSON.stringify(serialised, null, 4);
+                    console.log(json);
+                    done();
+                  });
+                }
+              }
+            ]
+          }
+        ],
+      },
+      Guide: [
+        {
+          type: 'markdown',
+          url: 'guide.md'
+        }
+      ]
+    }
+  },
+  highlight: function (code) {
+    return hljs.highlightAuto(code).value;
+  }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -5,37 +5,7 @@ var gulp = require('gulp'),
   plugins = require('gulp-load-plugins')();
 
 
-var webpack = {
-  externals: {
-    'react': 'React',
-    'jQuery': '$',
-    'react-router': 'ReactRouter',
-    'underscore': '_',
-    'moment': 'moment',
-    'async': 'async',
-    'marked': 'marked',
-    'react-bootstrap': 'ReactBootstrap'
-  },
-  devtool: 'inline-source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
-};
-
-gulp.task('build', [
-  'build:js',
-  'build:vendor',
-  'build:scss',
-  'build:html'
-]);
-
-gulp.task('build:scss', function () {
+gulp.task('build:brio:scss', function () {
   gulp.src('src/scss/main.scss')
     .pipe(plugins.sass().on('error', plugins.sass.logError))
     .pipe(plugins.autoprefixer({remove: true}))
@@ -43,49 +13,101 @@ gulp.task('build:scss', function () {
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('build:js', function () {
+gulp.task('build:brio:js', function () {
   return gulp.src('src/js/index.js')
-    .pipe(plugins.webpack(webpack))
+    .pipe(plugins.webpack({
+      externals: {
+        'react': 'React',
+        'jQuery': '$',
+        'react-router': 'ReactRouter',
+        'underscore': '_',
+        'moment': 'moment',
+        'async': 'async',
+        'marked': 'marked',
+        'react-bootstrap': 'ReactBootstrap'
+      },
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    }))
     .pipe(plugins.rename('bundle.js'))
     .pipe(gulp.dest('build'))
 });
 
-gulp.task('build:vendor', ['build:vendor:js', 'build:vendor:css']);
+gulp.task('build:demo:js', function () {
+  return gulp.src('demo/src/index.js')
+    .pipe(plugins.webpack({
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    }))
+    .pipe(plugins.rename('demo.js'))
+    .pipe(gulp.dest('build'))
+});
 
-gulp.task('build:vendor:css', function () {
+gulp.task('build:brio:vendor:css', function () {
   var vendorFiles = JSON.parse(fs.readFileSync('src/vendor.config.json', 'utf8'))['css'];
   return gulp.src(vendorFiles)
-    //.pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sourcemaps.init())
     .pipe(plugins.concat('vendor.css'))
-    //.pipe(plugins.sourcemaps.write())
+    .pipe(plugins.sourcemaps.write())
     .pipe(gulp.dest('build'))
 });
 
-gulp.task('build:vendor:js', function () {
+gulp.task('build:brio:vendor:js', function () {
   var vendorFiles = JSON.parse(fs.readFileSync('src/vendor.config.json', 'utf8'))['js'];
   return gulp.src(vendorFiles)
-    //.pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sourcemaps.init())
     .pipe(plugins.concat('vendor.js'))
-    //.pipe(plugins.sourcemaps.write())
+    .pipe(plugins.sourcemaps.write())
     .pipe(gulp.dest('build'))
 });
 
-gulp.task('build:html', function () {
-  return gulp.src('src/index.html')
-    .pipe(gulp.dest('build'));
-});
+gulp.task('build:js', [
+  'build:brio:js',
+  'build:demo:js'
+]);
+
+gulp.task('build:scss', [
+  'build:brio:scss'
+]);
+
+gulp.task('build:vendor', [
+  'build:brio:vendor:js',
+  'build:brio:vendor:css'
+]);
+
+gulp.task('build', [
+  'build:js',
+  'build:vendor',
+  'build:scss'
+]);
 
 gulp.task('serve', function () {
   plugins.connect.server({
-    root: 'build'
+    root: 'demo'
   });
 });
 
 gulp.task('watch', ['build', 'serve'], function () {
   plugins.livereload.listen();
-  gulp.watch('src/js/**/*.js', ['build:js']);
+  gulp.watch('src/js/**/*.js', ['build:brio:js']);
+  gulp.watch('demo/src/**/*.js', ['build:demo:js']);
   gulp.watch('src/scss/**/*.scss', ['build:scss']);
-  gulp.watch('src/**/*.html', ['build:html']);
   gulp.watch('src/vendor.config.json', ['build:vendor']);
 });
 
