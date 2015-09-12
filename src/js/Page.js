fc3c6adfed85be1e265b592237f84d2b2bb598ce
Michael Ford <mtford@gmail.com>
2015-09-12 11:21:40 +0100

    add hook for pages

--- src/js/Page.js
+++ src/js/Page.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import location from './location';
 
-export default class NavBarMenu extends React.Component {
+export default class Page extends React.Component {
   constructor(props) {
     super(props)
   }
 
   render() {
     return (
-      <div className="nav-menu">
+      <div className="page">
       </div>
     )
   }
